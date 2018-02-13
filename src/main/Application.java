057fc71cc41d03926333b94153b8b7d54e2de0a3
bko <dermont@web.de>
2018-02-13 20:42:46 +0100

    dont forget NPE

--- src/main/Application.java
+++ src/main/Application.java
@@ -10,21 +10,22 @@ import java.util.List;
 public class Application {
 
 
-    public static void main(String... args) throws InterruptedException {
+    public static void main(String... args) {
 
-        // Create Buildings and "Brandmeldezentrale" BMZ
+        // Create Buildings
         Building B1 = new Building("B1", 1, 2);
         Building B2 = new Building("B2", 2, 3);
         Building B3 = new Building("B3", 3, 1);
         Building B4 = new Building("B4", 4, 2);
         Building B5 = new Building("B5", 5, 2);
         Building B6 = new Building("B6", 6, 3);
-
-
+        //Create Mediator
         CentralFireDepartment CFD = new CentralFireDepartment();
+        //Register FireDepartment to Mediator
         FireDepartment F1 = new FireDepartment(CFD);
         FireDepartment F2 = new FireDepartment(CFD);
 
+        //Register FireAlarmControl "Brandmeldezentrale"
         B1.registerFireDep(F1);
         B2.registerFireDep(F1);
         B3.registerFireDep(F1);
@@ -41,12 +42,12 @@ public class Application {
         buildings.add(B5);
         buildings.add(B6);
 
-        for (int i = 0 ; i < 50;i++) {
+        for (int i = 0 ; i < 10;i++) {
             System.out.println(" New Alarm!");
             Collections.shuffle(buildings);
             for (Building building : buildings.subList(0, 2)) {
+                //needed concurrency unless sequential execution of method was wanted
                 new Thread(()-> building.randomFire()).start();
-
             }
             try {
 
